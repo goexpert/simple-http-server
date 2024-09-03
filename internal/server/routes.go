6f4066ebbc6031ef473ba9208bfab99f6df6ee77
Rogerio Tadim <rogeriotadim@gmail.com>
2024-09-03 01:42:03 +0000

    remove unused

--- internal/server/routes.go
+++ internal/server/routes.go
@@ -10,13 +10,12 @@ func (s *Server) RegisterRoutes() http.Handler {
 
 	mux := http.NewServeMux()
 	mux.HandleFunc("GET /", s.Healthz)
-	mux.HandleFunc("GET /api/v1/blueprint", s.GetBlueprints)
-	mux.HandleFunc("GET /api/v1/blueprint/{id}", s.GetBlueprintById)
 
 	return mux
 }
 
 func (s *Server) Healthz(w http.ResponseWriter, r *http.Request) {
+	log.Println("Healthz", r.RemoteAddr, r.RequestURI)
 	resp := make(map[string]string)
 	resp["message"] = "100%"
 
@@ -26,29 +25,4 @@ func (s *Server) Healthz(w http.ResponseWriter, r *http.Request) {
 	}
 
 	_, _ = w.Write(jsonResp)
-}
-
-func (s *Server) GetBlueprints(w http.ResponseWriter, r *http.Request) {
-	resp := make(map[string]string)
-	resp["blueprints"] = "100%"
-
-	jsonResp, err := json.Marshal(resp)
-	if err != nil {
-		log.Fatalf("error handling JSON marshal. Err: %v", err)
-	}
-
-	_, _ = w.Write(jsonResp)
-}
-
-func (s *Server) GetBlueprintById(w http.ResponseWriter, r *http.Request) {
-	id := r.PathValue("id")
-	resp := make(map[string]string)
-	resp["blueprints"] = id
-	jsonResp, err := json.Marshal(resp)
-	if err != nil {
-		log.Fatalf("error handling JSON marshal. Err: %v", err)
-	}
-
-	_, _ = w.Write(jsonResp)
-
 }
